4a747ac019febcab8c3d4fb1232af1b0385e7a40
Andy <att35663@uga.edu>
2018-10-12 20:19:18 -0400

    Redoing logic for storing info

--- WorkoutApp/src/pages/home/home.ts
+++ WorkoutApp/src/pages/home/home.ts
@@ -33,61 +33,44 @@ export class HomePage {
 
     this.setCounter = 0;
     this.repCounter = 0;
-    this.forCounter = 0;
-    this.addrow();
+    this.rsCounter = 0;
+
 
 
     }
 
   incrementReps() 
   {
-    this.rows[1].repCounter += 1;
+    this.rows[rsCounter].repCounter += 1;
   }
 
   incrementSets() 
   {
-    this.rows[1].setCounter += 1;
+    this.rows[rsCounter].setCounter += 1;
   }
 
   decrementReps() 
   {
-    this.rows[1].repCounter -= 1;
+    this.rows[rsCounter].repCounter -= 1;
   }
 
   decrementSets() 
   {
-    this.rows[1].setCounter -= 1;
+    this.rows[rsCounter].setCounter -= 1;
   }
 
-  editPrev() 
-  {
-    if((this.counter - 1) != -1)
-    {
-      this.counter -= 1;
-    }
-    
-  }
+  
 
   private addrow(){
 
-    console.log("addrow is called");
+    console.log("addrow is called")
 
     this.rows.push({ name: this.workout, setCounter: 0, repCounter : 0});
     
 
-    if(this.counter != -1){
-      this.copyRows.push({ name: this.workout, setCounter: 0, repCounter : 0});
-       console.log("Copying " + this.workout + " to this.copyRows")
-       console.log('this.forCounter is ' + this.forCounter);
-       this.copyRows[this.forCounter] = this.rows[1];
-       this.rows.pop();
-       this.forCounter++;
 
-    }
-    
 
     
-    this.counter += 1;
 
 
 
@@ -98,7 +81,7 @@ export class HomePage {
   }
 
   private printArray() {
-    console.log(this.copyRows);
+    console.log(this.rows);
 
 
 
