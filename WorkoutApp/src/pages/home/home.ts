4fe01b24e73a9612d9bb2b456bbb5c9e8dd27fec
Andy <att35663@uga.edu>
2018-10-12 20:05:23 -0400

    Modifying logic for storing workout info

--- WorkoutApp/src/pages/home/home.ts
+++ WorkoutApp/src/pages/home/home.ts
@@ -9,12 +9,13 @@ export class HomePage {
 
   public repetition : number;
   public sets : number;
+
   public setCounter : number;
   public repCounter : number;
-  public forCounter : number;
 
+  public forCounter : number;
   public counter : number;
-  public totalCards : number;
+
 
   public rows: Array<{ name: string, setCounter: number, repCounter : number}> = [];
   public copyRows: Array<{ name: string, setCounter: number, repCounter : number}> = [];
@@ -29,33 +30,33 @@ export class HomePage {
 
     this.sets = 0;
     this.counter = -1;
-    this.totalCards = 0;
+
     this.setCounter = 0;
     this.repCounter = 0;
     this.forCounter = 0;
-    console.log("This.counter is " + this.counter);
     this.addrow();
+
 
     }
 
   incrementReps() 
   {
-    this.rows[0].repCounter += 1;
+    this.rows[1].repCounter += 1;
   }
 
   incrementSets() 
   {
-    this.rows[0].setCounter += 1;
+    this.rows[1].setCounter += 1;
   }
 
   decrementReps() 
   {
-    this.rows[0].repCounter -= 1;
+    this.rows[1].repCounter -= 1;
   }
 
   decrementSets() 
   {
-    this.rows[0].setCounter -= 1;
+    this.rows[1].setCounter -= 1;
   }
 
   editPrev() 
@@ -69,30 +70,26 @@ export class HomePage {
 
   private addrow(){
 
+    console.log("addrow is called");
+
+    this.rows.push({ name: this.workout, setCounter: 0, repCounter : 0});
+    
 
     if(this.counter != -1){
+      this.copyRows.push({ name: this.workout, setCounter: 0, repCounter : 0});
        console.log("Copying " + this.workout + " to this.copyRows")
-       
-       this.copyRows[this.forCounter] = this.rows[0];
-       this.copyRows[0].name = this.workout;
+       console.log('this.forCounter is ' + this.forCounter);
+       this.copyRows[this.forCounter] = this.rows[1];
        this.rows.pop();
        this.forCounter++;
 
     }
     
-    this.rows.push({ name: this.workout, setCounter: 0, repCounter : 0});
-    this.copyRows.push({ name: this.workout, setCounter: 0, repCounter : 0});
+
+    
+    this.counter += 1;
 
 
-    if(this.counter + 1 != this.totalCards)
-    {
-      this.counter = this.totalCards;
-    }
-    else
-    {
-      this.counter += 1;
-      this.totalCards += 1;
-    }
 
 
 
